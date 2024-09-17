cd7927935e86292a6fc6d18065e084ab3efe9877
web3degenerate <hostnomics@gmail.com>
2024-09-16 20:07:27 -0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Test https://www.youtube.com/watch?v=buMTH6ICnqk&list=PL0-84-yl1fUnRuXGFe_F7qSH1LEnn9LkW&index=3
+
+print('Hellow World')
+print ('What is your name?')
+yourName = input()
+print('Good to meet you, ' + yourName)
