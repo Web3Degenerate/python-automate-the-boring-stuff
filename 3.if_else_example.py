0ad6662fb099bd97c64901db617ae7ccf9f32326
web3degenerate <hostnomics@gmail.com>
2024-09-18 23:21:24 -0700

    Added 3.if_else_example.py with elif examples

---
+++ 3.if_else_example.py
@@ -0,0 +1,22 @@
+password = 'swordfish'
+
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
+
+
+
+name = 'Bob'
+age = 3000
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice kiddo')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead importal vampire')
+elif age >100:
+    print('You are not Alice granny')
+
+    
