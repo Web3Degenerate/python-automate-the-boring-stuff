4717822872c0fc4385aa2c5ea42085712a3d9cac
web3degenerate <hostnomics@gmail.com>
2024-10-01 23:54:24 -1200

    Updated 10.string-formatter.py and added README.md

--- 10.string-formatter.py
+++ 10.string-formatter.py
@@ -12,3 +12,15 @@ def format_name(f_name, l_name):
 
 formatted_string = format_name('edward', 'McDooGle')
 print(formatted_string)
+
+
+############ Pass another function as the parameter in the function call: #########
+
+def function_1(text):
+    return text + text
+
+def function_2(text):
+    return text.title()
+
+output = function_2(function_1('hello'))
+print(output)
---
+++ README.md
@@ -0,0 +1,17 @@
+### List of helpful functions
+
+1. len()
+
+
+2. str() and int()
+
+...
+
+
+3. title() - Uppercase first letter of each word
+```py
+
+def function(text):
+    return text.title()
+
+```
