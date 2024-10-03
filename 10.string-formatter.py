4dc4c352d273c90f8c0dec0274be8d4794584e3f
web3degenerate <hostnomics@gmail.com>
2024-10-02 22:22:12 -1200

    Updated 10.string-formatter.py

--- 10.string-formatter.py
+++ 10.string-formatter.py
@@ -2,17 +2,22 @@
 
 ################## title() function ###################
 def format_name(f_name, l_name):
-    # print(f_name.title())
-    # print(l_name.title())
+        # print(f_name.title())
+        # print(l_name.title())
+
+    # if either input is missing, escape the function:    
+    if f_name == "" or l_name == "":
+        # return #returns #none
+        return "You did not return a valid input."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     # print(f"Hello {formatted_f_name} {formatted_l_name}, how are you?")
 ## USE RETURN INSTEAD
     return f"Hello {formatted_f_name} {formatted_l_name}, how are you?"
 
-formatted_string = format_name('edward', 'McDooGle')
-print(formatted_string)
-
+# formatted_string = format_name('edward', 'McDooGle')
+# print(formatted_string)
+print(format_name(input("What is your first name?"), input("What is your last name?")))
 
 ############ Pass another function as the parameter in the function call: #########
 
