6c2b35f06b3748b6aed73b00a1e41be2d4c5ff34
web3degenerate <hostnomics@gmail.com>
2024-10-19 13:00:21 -0500

    Added calculate_score method to 11.black-jack/main.py

--- 11.black-jack/main.py
+++ 11.black-jack/main.py
@@ -6,8 +6,24 @@ def deal_card():
     card = random.choice(cards)
     return card
 
+def calculate_score(cards):
+    """Takes a list of cards and returns the score calculated from the cards"""
+    """ Check for 11, 10 and length of 2, ie blackjack with 2 cards"""
+    # if 11 in cards and 10 in cards and len(cards) == 2:
+    """ refactor """
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    """ Check for changing 11 to 1 """
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 """If you don't need a variable you can use an underscore _ """
 for _ in range(2):
@@ -27,5 +43,11 @@ for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-def calculate_score(cards):
-    sum(cards)
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards are {user_cards} and your score is {user_score}")
+print(f"Dealer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score >21:
+    is_game_over = True
