7e2cb06af32349d38803b422a0f4da52cd15c06f
web3degenerate <hostnomics@gmail.com>
2024-10-29 12:34:11 -0500

    Wrapped game logic in play_game function in 11.black-jack/main.py

--- 11.black-jack/main.py
+++ 11.black-jack/main.py
@@ -1,4 +1,6 @@
 import random
+# to import the ascii art: 
+# from art import logo
 
 def deal_card():
     """Returns a random card from the deck"""
@@ -38,53 +40,62 @@ def compare(u_score, c_score):
     else:
         return "Dealer wins...again."
 
-user_cards = []
-computer_cards = []
-computer_score = -1 #to prevent computer_score from being undefined
-user_score = -1 #to prevent user_score from being undefined. 
-is_game_over = False
+#Finally, wrap our game code in it's own function for the final while loop (game continuation) at the bottom. 
+def play_game():
+    #to import ASCII art at start of game: print(logo)
+    user_cards = []
+    computer_cards = []
+    computer_score = -1 #to prevent computer_score from being undefined
+    user_score = -1 #to prevent user_score from being undefined. 
+    is_game_over = False
 
-"""If you don't need a variable you can use an underscore _ """
-for _ in range(2):
-    """ NOTE: += is the same thing as .extend() which expects a list
-    user_cards += new_card 
-    user_cards.extend(new_card)
-    Returns TYPE ERROR: `int object not iterable`
-    
-    To fix you could do
-    new_card = [deal_card()]  => call the function into a list
-    user_card += new_card
-    """
-    """Returns a random card from the deck"""
-    # new_card = deal_card()
-    # user_cards.append(new_card)
-    """refactor: """
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+    """If you don't need a variable you can use an underscore _ """
+    for _ in range(2):
+        """ NOTE: += is the same thing as .extend() which expects a list
+        user_cards += new_card 
+        user_cards.extend(new_card)
+        Returns TYPE ERROR: `int object not iterable`
+        
+        To fix you could do
+        new_card = [deal_card()]  => call the function into a list
+        user_card += new_card
+        """
+        """Returns a random card from the deck"""
+        # new_card = deal_card()
+        # user_cards.append(new_card)
+        """refactor: """
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-while not is_game_over: 
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"Your cards are {user_cards} and your score is {user_score}")
-    print(f"Dealer's first card: {computer_cards[0]}")
+    while not is_game_over: 
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards are {user_cards} and your score is {user_score}")
+        print(f"Dealer's first card: {computer_cards[0]}")
 
-    if user_score == 0 or computer_score == 0 or user_score >21:
-        is_game_over = True
-    else:
-        user_should_deal = input("Type 'y' to get another card. Type 'n' to pass: ")
-        if user_should_deal == "y":
-            user_cards.append(deal_card())
-        else:
+        if user_score == 0 or computer_score == 0 or user_score >21:
             is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card. Type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-# (12). Computer should draw if less than 17, but stop when 21 or over
-while computer_score != 0 and computer_score <17:
-    computer_cards.append(deal_card())
-    """computer_score is defined in while loop above. If no value, undefined"""
-    computer_score = calculate_score(computer_cards)
+    # (12). Computer should draw if less than 17, but stop when 21 or over
+    while computer_score != 0 and computer_score <17:
+        computer_cards.append(deal_card())
+        """computer_score is defined in while loop above. If no value, undefined"""
+        computer_score = calculate_score(computer_cards)
 
 
-#Final comparing of user vs dealer score: 
-print(f"Your final hand {user_cards}, Your hand total: {user_score}")
-print(f"Dealer final hand {computer_cards}, Dealer hand total: {computer_score}")
-print(compare(user_score, computer_score))
+    #Final comparing of user vs dealer score: 
+    print(f"Your final hand {user_cards}, Your hand total: {user_score}")
+    print(f"Dealer final hand {computer_cards}, Dealer hand total: {computer_score}")
+    print(compare(user_score, computer_score))
+
+
+# prompt the user to play another hand: 
+while input("Do you want to play another hand? Type 'y' or 'n': ") == "y":
+    print("\n" * 20)
+    play_game()
