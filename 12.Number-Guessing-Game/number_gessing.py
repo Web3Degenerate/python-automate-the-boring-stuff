d1d7006266755084fc5515da2a630100db5d98e1
web3degenerate <hostnomics@gmail.com>
2024-11-05 23:59:49 -0600

    Updated 12.Number-Guessing-Game/number_guessing.py

--- 12.Number-Guessing-Game/number_gessing.py
+++ 12.Number-Guessing-Game/number_gessing.py
@@ -10,9 +10,17 @@ answer = randint(1, 100)
 
 
 # Function to set difficulty
+def check_answer(user_guess, actual_answer):
+    if user_guess > actual_answer: 
+        print("Your guess is too high, guess lower")
+    elif user_guess < actual_answer:
+        print("Too low, guess higher")
+    else:
+        print(f"You guessed it! The answer was {actual_answer}")
 
 
 # Let user guess a number
+guess = int(input("Make a guess: "))
 
 
 # Check number against original number.
