72e57200e47ebf072dd9cc48c5bd3e1933f2c9af
web3degenerate <hostnomics@gmail.com>
2024-11-11 23:54:30 -1200

    imported game_data.py and highlowart.py into main.py in directory 14.Higher-Lower-Game

---
+++ 14.Higher-Lower-Game/main.py
@@ -0,0 +1,44 @@
+
+# Display Art
+from highlowart import logo
+print(logo)
+
+# Import game_data
+from game_data import data
+
+# Generate a random account from game_data.py
+import random 
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+
+if account_a == account_b:
+    account_b = random.choice(data)
+
+# Ask user for a guess
+
+
+
+# Check if user is correct
+
+
+
+# Get follower account of each account
+
+
+# Use if statement to check if user is correct
+
+
+# Give user feedback on their guess
+
+
+
+# score keeping
+
+
+
+# Make the game repeatable
+
+
+
+# Making account at position B become the next account position A
