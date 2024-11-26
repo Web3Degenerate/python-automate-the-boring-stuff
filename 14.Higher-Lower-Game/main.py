8fd357850aebbfc691398ebbfa05f3a99ff7eadb
web3degenerate <hostnomics@gmail.com>
2024-11-25 23:58:42 -1200

    Completed 14.Higher-Lower-Game/main.py

--- 14.Higher-Lower-Game/main.py
+++ 14.Higher-Lower-Game/main.py
@@ -36,58 +36,50 @@ def check_answer(user_guess, a_followers, b_followers):
         return user_guess == "b"
 
 
-account_a = random.choice(data)
+score = 0
+game_should_continue = True
 account_b = random.choice(data)
 
-if account_a == account_b:
-    #set account b to a random selection from our data set
+while game_should_continue:
+    # Making account at position B become the next account position A
+    account_a = account_b
     account_b = random.choice(data)
 
-
-print(f"Comare A: {format_data(account_a)}.")
-print(vs)
-print(f"Against B: {format_data(account_b)}.")
-
-# Ask user for a guess
-guess = input("Who has more followers, Type 'A' or 'B': ").lower()
+    if account_a == account_b:
+        #set account b to a random selection from our data set
+        account_b = random.choice(data)
 
 
+    print(f"Comare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
 
-## - Get Follower count for each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    # Ask user for a guess
+    guess = input("Who has more followers, Type 'A' or 'B': ").lower()
 
+    #Clear the screen
+    print("\n *20")
+    print(logo)
 
-# Check if user is correct
-is_correct = check_answer(guess, a_follower_count, b_follower_count) #true or false
-
-# Give user feedback on their guess
-# Score keeping
-score = 0
-if is_correct: 
-    score += 1
-    print(f"You're right. Current score {score}")
-else: 
-    print(f"Sorry, that's wrong. Your Final Score: {score}")
-
-## - Set up check answer function above
-
-# Get follower account of each account
+    ## - Get Follower count for each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
 
-# Use if statement to check if user is correct
+    # Check if user is correct
+    is_correct = check_answer(guess, a_follower_count, b_follower_count) #true or false
 
+    # Give user feedback on their guess
+    # Score keeping
 
-# Give user feedback on their guess
+    if is_correct: 
+        score += 1
+        print(f"You're right. Current score {score}")
+    else: 
+        print(f"Sorry, that's wrong. Your Final Score: {score}")
+        game_should_continue = False
 
 
 
-# score keeping
 
 
-
-# Make the game repeatable
-
-
-
-# Making account at position B become the next account position A
