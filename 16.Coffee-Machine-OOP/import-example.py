f82a9936350ea0ff22f1f9e76b49887572375856
hostnomics <hostnomics@gmail.com>
2024-12-13 23:59:04 -1200

    Imported turtle module and created Turtle object in 16.Coffee-Machine-OOP/import-example.py

--- 16.Coffee-Machine-OOP/import-example.py
+++ 16.Coffee-Machine-OOP/import-example.py
@@ -2,3 +2,15 @@ import import_module_example
 
 print(import_module_example.example_variable)
 
+# Then import a class `Turtle` from external Turtle module to create Turtle Object
+import turtle
+
+
+#Alternatively, use from - import just the Turtle class
+from turtle import Turtle
+
+# Use parenthesis to actually construct the turtle object
+timmy = turtle.Turtle()
+
+print(timmy) #prints object <turtle.Turtle object at 0x0000026AD0848F10>
+
