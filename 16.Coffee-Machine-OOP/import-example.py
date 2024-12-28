e67a78c4904de59d00691ee25a29456e54934980
web3degenerate <hostnomics@gmail.com>
2024-12-27 23:54:21 -1200

    Updated 16.Coffee-Machine-OOP/import-example.py

--- 16.Coffee-Machine-OOP/import-example.py
+++ 16.Coffee-Machine-OOP/import-example.py
@@ -7,10 +7,14 @@ import turtle
 
 
 #Alternatively, use from - import just the Turtle class
+# import turtle
 from turtle import Turtle
 
 # Use parenthesis to actually construct the turtle object
-timmy = turtle.Turtle()
+# using from turtle import Turtle (instead of just `import turtle` 
+# changes how we can call the Turtle() obejct):
+# timmy = turtle.Turtle()
+timmy = Turtle()
 
-print(timmy) #prints object <turtle.Turtle object at 0x0000026AD0848F10>
+print(timmy) #prints object <turtle.Turtle object at 0x0000026AD0848F10> (location saved at in computer's memory)
 
