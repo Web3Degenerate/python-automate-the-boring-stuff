80a00b5e68137aca072886b562054278b70a1480
web3degenerate <hostnomics@gmail.com>
2025-01-02 20:49:03 -1200

    Updated 16.Coffee-Machine-OOP/import-example.py

--- 16.Coffee-Machine-OOP/import-example.py
+++ 16.Coffee-Machine-OOP/import-example.py
@@ -8,7 +8,7 @@ import turtle
 
 #Alternatively, use from - import just the Turtle class
 # import turtle
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 # Use parenthesis to actually construct the turtle object
 # using from turtle import Turtle (instead of just `import turtle` 
@@ -17,6 +17,13 @@ from turtle import Turtle
 timmy = Turtle()
 
 print(timmy) #prints object <turtle.Turtle object at 0x0000026AD0848F10> (location saved at in computer's memory)
+
+#print actual turtle
+timmy.shape("turtle")
+timmy.color("coral")
+
+#forward method moves turtle icon
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
