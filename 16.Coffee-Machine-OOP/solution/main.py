fe76e55eea5d88d52ee6af75df51946d564e495d
web3degenerate <hostnomics@gmail.com>
2025-01-06 23:53:31 -1200

    Updated 16.Coffee-Machine-OOP/solution/main.py

--- 16.Coffee-Machine-OOP/solution/main.py
+++ 16.Coffee-Machine-OOP/solution/main.py
@@ -1,3 +1,12 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+
+coffee_maker = CoffeeMaker()
+
+coffee_maker.report()
+money_machine.report()
+
+#resume at Sec 16, v.114 at (3:53)
