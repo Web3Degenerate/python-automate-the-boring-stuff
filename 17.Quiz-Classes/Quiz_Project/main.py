0a1ae8809456aa94bf76d727d85ed8d64b95327a
web3degenerate <hostnomics@gmail.com>
2025-02-02 08:25:29 -0800

    Imported question_model.py Question class and data.py into main.py and set up the Quiz question_bank in director 17.Quiz-Classes/Quiz_Project

---
+++ 17.Quiz-Classes/Quiz_Project/main.py
@@ -0,0 +1,35 @@
+from question_model import Question
+from data import question_data
+
+
+# Loop through the question_data list of dictionaries and add to our question_bank LIST (array)
+
+"""1. Write a for loop to iterate over the question_data"""
+
+"""
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+"""
+
+
+
+question_bank = [] #Create List (array)
+for x in question_data:
+    x_text = x["text"]
+    x_answer = x["answer"]
+    """2. Create a Question object from each entry in question_data"""
+    new_question = Question(x_text, x_answer)
+    """3. Append each Question object to the question_bank"""
+    question_bank.append(new_question)
+
+
+# print(question_bank) #will return a list of question objects [<Object>, <Object>, etc.]
+
+# Drill down into List of Questions Objects: 
+print(f'First Question: {question_bank[0].text} \n First Answer: {question_bank[0].answer}')
+
+
+# Pick up in Sec 17 - V.122
--- 17.Quiz-Classes/Quiz_Project/question_model.py
+++ 17.Quiz-Classes/Quiz_Project/question_model.py
@@ -1,9 +1,9 @@
 class Question: 
-    def __init(self, text, answer): 
+    def __init__(self, text, answer): 
         self.text = text
         self.answer = answer
 
 
 # example call 
-new_question = Question("Capital Pennsylvania", "Harrisburg")
-new_question.text #Capital Pennsylvania
+# new_question = Question("Capital Pennsylvania is Harrisburg", "True")
+# new_question.text #Capital Pennsylvania is Harrisburg
