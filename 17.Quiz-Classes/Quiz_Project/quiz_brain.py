f13ee7d935053f66fba3863a0d98afc5f5530693
web3degenerate <hostnomics@gmail.com>
2025-02-05 10:58:07 -0600

    Added still_has_questions() method to QuizBrain class in quiz_brain.py for the while loop in main.py in directory 17.Quiz-Classes/Quiz_Project/

--- 17.Quiz-Classes/Quiz_Project/quiz_brain.py
+++ 17.Quiz-Classes/Quiz_Project/quiz_brain.py
@@ -8,6 +8,22 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
+
+    def still_has_questions(self): 
+        #returns boolean depending on the value of question_number
+        """Use len() method to dynamically calculate size of question_data instead of hardcoded at 12"""
+            # if self.question_number > 12: 
+            #     return false
+            # else: 
+            #     return true
+        """Simply these four lines of code into one below"""    
+        # if self.question_number < len(self.question_list):
+        #     return True
+        # else:
+        #     False
+        return self.question_number < len(self.question_list)
+
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         #So current_question is a Question object and has text and question attributes
@@ -18,3 +34,6 @@ class QuizBrain:
         #or better solution, += 1
         self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+
+
+
