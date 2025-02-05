08f7b6dcf8794b0c7193e9691acac4aca4f8819a
web3degenerate <hostnomics@gmail.com>
2025-02-05 11:30:50 -0800

    Added check_answer function to quiz_brain.py and completed quiz project in main.py in directory 17.Quiz-Classes/Quiz_Project/

--- 17.Quiz-Classes/Quiz_Project/main.py
+++ 17.Quiz-Classes/Quiz_Project/main.py
@@ -41,3 +41,7 @@ quiz = QuizBrain(question_bank)
 # while quiz.still_has_questions: # FORGOT () FOR METHOD!!
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("****You have completed the Quiz*******\n")
+#alternative: {len(question_bank)}
+print(f"Your final score was {quiz.user_score}/{quiz.question_number}")
