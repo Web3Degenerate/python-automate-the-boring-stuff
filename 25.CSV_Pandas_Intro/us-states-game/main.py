24acc3cd4c57d8ca2b4684caac20fe72f9e9b720
web3degenerate <hostnomics@gmail.com>
2025-02-14 18:04:24 -0600

    Imported pandas into /25.CSV_Pandas_Intro/us-states-game/main.py and imported state data

--- 25.CSV_Pandas_Intro/us-states-game/main.py
+++ 25.CSV_Pandas_Intro/us-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,10 +8,23 @@ screen.addshape(image)
 turtle.shape(image)
 
 
+#Sec 25. V. 194. Use pandas' read_csv()
+# columns state, x, y
+data = pandas.read_csv("50_states.csv")
+
 
 # Get user input: 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another State's name?")
 print(answer_state)
+
+
+# Check If answer_state is one of the states in our 'state' column
+    #If so (they got it right):
+        #Create a turtle to write the name of the state @ the state's X, Y coordinates
+        
+
+
+
 
 
 '''Solution if we didn't already have the x, y coordinates in our csv. Just an FYI'''
