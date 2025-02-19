0159819c3e21c08e57f62d1009fa76d1220687ae
web3degenerate <hostnomics@gmail.com>
2025-02-18 19:34:44 -0600

    Added /26.List-Comprehension/iterate-pandas-dataframe.py

---
+++ 26.List-Comprehension/iterate-pandas-dataframe.py
@@ -0,0 +1,47 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+for (key, value) in student_dict.items(): 
+    print(key)
+    # print(f"fucking dipshit student {key} scored a bloddy {value} on their exam!")
+
+
+#=============================================================================================
+#======= Iterating over Pandas DataFrame PRETTY MUCH the same as Python Dict =================
+#=============================================================================================
+
+import pandas
+
+# Create new DataFrame from our student_dict
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+
+#Loop through a data frame the ole fashion way = MESSY
+for (key, value) in student_data_frame.items(): 
+    print(value) 
+
+
+
+#=============================================================================================
+#======= Pandas DataFrame IN BUILT LOOP =================
+#=============================================================================================
+# Sec 26. V. 200
+
+'''    .iterrows()   '''
+for (index, row) in student_data_frame.iterrows(): 
+    print(index) #prints DataFrame, then each index number
+    
+    print(row) #prints out each row, bit sloppy tho
+
+    '''Each row is a Pandas Series Object, so we can access info with dot notation'''
+    print(row.student) #prints out DataFrame, then each student value (Angela, James Lilly)
+
+    print(row.score) #same for the score values
+
+    '''Use Conditional logic to only print Angela's Score'''
+    if row.student == "Angela": 
+        print(row.score)
+
