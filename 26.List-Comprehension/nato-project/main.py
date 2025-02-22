300ae360d79ce084b27031e8c579fac15e23ef8a
web3degenerate <hostnomics@gmail.com>
2025-02-22 16:17:04 -0600

    Completed /26.List-Comprehension/nato-project/main.py

--- 26.List-Comprehension/nato-project/main.py
+++ 26.List-Comprehension/nato-project/main.py
@@ -1,10 +1,18 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
+# print(data.to_dict())
 
 #TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 # format: 
 # {row.letter:new_value for (index, row) in data.iterrows()}
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)
+
+
+#TODO 2. Take user input and check it against each key in the dictionary
+word = input("enter a word: ").upper()
+# Format: 
+# [new_item for item in list]
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
