f5c37bf229f77c94f96db3c4df059d59061ba904
web3degenerate <hostnomics@gmail.com>
2025-03-03 20:13:42 -0600

    Updated /27.tkinter-create-GUI/main.py with tkinter Button and Entry classes

--- 27.tkinter-create-GUI/main.py
+++ 27.tkinter-create-GUI/main.py
@@ -1,4 +1,5 @@
 # tkinter comes with python
+# to import all classes use `import tkinter *`
 import tkinter
 
 window = tkinter.Tk()
@@ -18,13 +19,47 @@ my_label.pack() #default "top" center
 my_label.pack(side="bottom", expand=True) #expand take up all space it can
 
 
+#using tkinter, we can set text with either way: 
+my_label['text'] = "New Text"
+my_label.config(text="Newer Text")
+
+
+#===================================================
+# ==== Buttons with custom click functions ================
+# Pass to window in the Button() function, param `command=<funciton>`
+# ==============================================
+
+def button_clicked(): 
+    print("I got clicked") #prints text to the command line
+    user_input = input.get()
+    # my_label.config(text="I got clicked")
+    my_label.config(text=user_input)
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
 
 
 #===================================================
 # ==== **kw - kwargs Optional Arguemnts ================
+# Reviewed in S.27. V.209. 
 # ==============================================
 
-# Resume S.27. V.209. 
+
+#===================================================
+# ==== Entry Class ================
+# Input Fields - used KWARGS with .get() in S.27.V.209
+# ==============================================
+
+
+input = tkinter.Entry(width=10)
+input.pack()
+input.get() #so I can optionally tap into kwargs I want without throwing error on skipped ones
+
+
+
+
 
 
 
