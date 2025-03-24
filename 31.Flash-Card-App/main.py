e557039d569ac10cc2cadda3614fc175746f3d1d
web3degenerate <hostnomics@gmail.com>
2025-03-24 01:57:34 -0500

    Updated /31.Flash-Card-App/main.py

--- 31.Flash-Card-App/main.py
+++ 31.Flash-Card-App/main.py
@@ -6,15 +6,33 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv") #dataframe
 to_learn = data.to_dict(orient="records") # records list like [{column -> value},...,{column->value}]
-print(to_learn)
+# print(to_learn)
+
+## Sec 31, Vid 240 - (2:15) - set current_card empty dictionary to pass from next_card() to flip_card()
+current_card = {}
 
 #add next_card function in Sec 31, vid 238 at 00:45)
 def next_card():
+  global current_card, flip_timer
+  window.after_cancel(flip_timer) #pass in the id of our flip_timer (7:40 Sec 31, Vid 240)
   # pass
   current_card = random.choice(to_learn)
   # print(current_card["French"])
-  canvas.itemconfig(card_title, text="French")
-  canvas.itemconfig(card_word, text=current_card["French"])
+  canvas.itemconfig(card_title, text="French", fill="black")
+  canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+  # Sec 31, Vid 240 - (5:20) Set the French side back to white background and black text
+  canvas.itemconfig(card_background, image=card_front_img)
+  # Sec 31, Vid 240 - (6:05) - Add 3 second flip for EACH card
+  # window.after(3000, func=flip_card)
+  # Sec 31, Vid 240 - 7:45 - set up NEW flip_timer to wait for 3 seconds
+  flip_timer = window.after(3000, func=flip_card)
+
+def flip_card(): ## Sec 31, Vid 240 - (1:15) - function to flip card after 3 seconds
+  canvas.itemconfig(card_title, text="English", fill="white")
+  canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+  # Sec 31, Vid 240 (4:00)
+  canvas.itemconfig(card_background, image=card_back_img)
+
 
 window = Tk()
 window.title("Flashy")
@@ -23,11 +41,21 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # my_image = PhotoImage(file="images/card_front.png")
 # button = Button(image=my_image, highlightthickness=0)
 
+# Sec 31, Vid 240 - (00:40) - Set 3 second timer for card to flip
+# window.after(3000, func=flip_card)
+# Sec 31, Vid 240 - (7:10) - change to global and add to global variables in next_card() function
+flip_timer = window.after(3000, func=flip_card)
+
 
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+
+# Sec 31, Vid 240 - (3:05) - set to card_background variable 
+# You have to create card_back_img down here, not in a funciton above for it to work (3:40)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+
 #Add some text to our card
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
