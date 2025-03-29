82fdc39fc03420adb5b7926f0389e1dfeb5603ef
web3degenerate <hostnomics@gmail.com>
2025-03-29 17:50:25 -0500

    Added /21.Snake-Game-2-Inheritance-List-Slicing/Snake-Game-Food-Collision-154/food.py

---
+++ 21.Snake-Game-2-Inheritance-List-Slicing/Snake-Game-Food-Collision-154/food.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+
+class Food:
+    def __init__():
+        pass
+        # instead of creating our turtle object as an attribute of this class
+        # like this: self.food = Turtle()
+        '''we want the Food class to inherit from the Turtle Class (Sec 21. Vid 154 (~1:45))'''
+        
