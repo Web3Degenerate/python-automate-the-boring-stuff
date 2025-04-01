309febb1ae79a28b30e6adc17dacb944782ad858
web3degenerate <hostnomics@gmail.com>
2025-03-31 20:28:52 -0500

    Updated /21.Snake-Game-2-Inheritance-List-Slicing/Snake-Game-Food-Collision-154/154-main.py, food.py and snake.py

--- 21.Snake-Game-2-Inheritance-List-Slicing/Snake-Game-Food-Collision-154/food.py
+++ 21.Snake-Game-2-Inheritance-List-Slicing/Snake-Game-Food-Collision-154/food.py
@@ -1,16 +1,16 @@
 from turtle import Turtle
-
+import random
 
 '''we want the Food class to inherit from the Turtle Class (Sec 21. Vid 154 (~1:45))'''
 # class Food:
-#     def __init__():
+#     def __init__(self):
 #         pass
         # instead of creating our turtle object as an attribute of this class
         # like this: self.food = Turtle()
 
 class Food(Turtle):
 
-    def __init__():
+    def __init__(self):
         super().__init__() #gives you access to all the Turtle methods
         #example shape()
         self.shape("circle")
@@ -18,3 +18,7 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5) #creates 10 x 10 circle. Default is 20 x 20.
         self.color("blue")
         self.speed("fastest")
+        #set random x and y coordinates with the random module
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y) #go to a random x,y coordinate
