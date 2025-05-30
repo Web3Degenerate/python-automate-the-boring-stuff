6623c52f91500f523f9e7016a8e1f3adf8cfc788
web3degenerate <hostnomics@gmail.com>
2025-05-29 18:17:58 -0700

    Updated scoreboard.py in /24.Files-Directories-and-Paths/

--- 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/scoreboard.py
+++ 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/scoreboard.py
@@ -17,13 +17,22 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}, | High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    '''Sec 24. V182 (1:21) Replace game_over method with reset()'''
+    def reset(self):
+        #check user score against high_score. If so, update high_score 
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0 #reset score
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+        # self.clear() # move to update_scoreboard
         self.update_scoreboard()
