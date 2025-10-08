c62fdd35cadb900011920b62d39b1bb417d6ddc1
web3degenerate <hostnomics@gmail.com>
2025-10-08 00:00:48 -1200

    Updated main_test.py

--- 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
+++ 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
@@ -1,16 +1,16 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
 
-# close the file after finished so it doesn't take up additional resources. 
-file.close() #manually close the file. 
+
+# file.close() #manually close the file. # close the file after finished so it doesn't take up additional resources. 
 
 # Avoid having to use open and close with the keywords `with` and `as`
 
-with open("my_file.txt") as file
-# file = open("my_file.txt")
-contents = file.read()
-print(contents)
+with open("my_file.txt") as file:
+    # file = open("my_file.txt")
+    contents = file.read()
+    print(contents)
 
 # close the file after finished so it doesn't take up additional resources. 
-file.close() #manually close the file. 
+# file.close() #manually close the file. 
