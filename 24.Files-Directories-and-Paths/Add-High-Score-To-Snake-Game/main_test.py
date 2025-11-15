6e65a2ea499bbc4dcaf0b5b2fa9d6f7cd0ef57ef
web3degenerate <hostnomics@gmail.com>
2025-11-14 23:07:58 -1200

    Updated 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py with examples of using open in append mode and write mode.

--- 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
+++ 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
@@ -9,17 +9,28 @@
 
 with open("my_file.txt") as file:
     # file = open("my_file.txt")
-    contents = file.read()
+    contents = file.read() #save contents in variable `contents`
     print(contents)
 
 # close the file after finished so it doesn't take up additional resources. ls
 # file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file. 
-# file.close() #manually close the file.
-# file.close() #manually close the file.
 
+
+## WRITE to file
+# by default open("file.txt", mode=r), thus read only
+# with open("my_file.txt") as file: #this opens the file in read-only mode
+#     file.write("New text.")
+
+# Change the mode to editable with mode=w
+# This overwrites the old text
+# with open("my_file.txt", mode="w") as file: #this opens the file in read-only mode
+#     file.write("New REPLACEMENT text.")
+
+# Append Mode
+with open("my_file.txt", mode="a") as file: #this opens the file in read-only mode
+    file.write("\nNew APPENDED text.")
+
+#
+# IF YOU OPEN a file that doesn't exist IN WRITE (mode=w) mode, it will create it for you
+with open("new_file.txt", mode="w") as file: #this opens the file in read-only mode
+    file.write("Created with open() in mode=w aka write mode.")
