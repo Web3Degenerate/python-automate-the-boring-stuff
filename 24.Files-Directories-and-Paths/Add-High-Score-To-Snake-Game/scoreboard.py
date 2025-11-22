d43eeec619e88a71448d88403420638ee2a150d4
web3degenerate <hostnomics@gmail.com>
2025-11-22 06:05:59 -0600

    Updated scoreboard.py to persist snake game highscore between sessions in /24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game

--- 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/scoreboard.py
+++ 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/scoreboard.py
@@ -11,8 +11,9 @@ class Scoreboard(Turtle):
         '''Sec 24. V.182 (1:00) Add attribute self.high_score'''
         '''Sec 24. V184 (2:05) Remove self.high_score and read from data.txt'''
         # self.high_score = 0
+        '''use with keyword so we don't have to worry about closing the file. Python will manage it for us.'''
         with open("data.txt") as data:
-            int(data.read())
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -28,6 +29,9 @@ class Scoreboard(Turtle):
         #check user score against high_score. If so, update high_score 
         if self.score > self.high_score:
             self.high_score = self.score
+            '''Sec 24. V184 (3:45) Update high score in data.txt'''
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0 #reset score
         self.update_scoreboard()
 
