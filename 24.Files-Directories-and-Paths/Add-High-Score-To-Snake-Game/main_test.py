b6014f815bb364a9c7535020ff04c62f668367c9
web3degenerate <hostnomics@gmail.com>
2025-12-25 06:42:29 -0600

    Attempted relative path with main_test.py in directory 24

--- 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
+++ 24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/main_test.py
@@ -40,6 +40,21 @@ with open("my_file.txt", mode="a") as file: #this opens the file in read-only mo
 with open("new_file.txt", mode="w") as file: #this opens the file in read-only mode
     file.write("Created with open() in mode=w aka write mode.")
 
+# Absolute Path to `new_file.txt`
 with open("C:/Users/dAppH/OneDrive/Documents/Dev/python/automate-the-boring-stuff/python-automate-the-boring-stuff/24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/new_file.txt") as file:
     contents = file.read()
     print(contents)
+
+# Relative Path to `new_file.txt`
+# Couldn't get it to work with relative path
+with open("/Users/dAppH/OneDrive/Documents/Dev/python/automate-the-boring-stuff/python-automate-the-boring-stuff/24.Files-Directories-and-Paths/Add-High-Score-To-Snake-Game/new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+
+# TRIED MOVING TO ACTUAL DESKTOP - OUTSIDE OF OneDrive
+# with open("C:\Users\dAppH\OneDrive\Desktop\new_file.txt") as file:
+# with open("C:/Users/dAppH/Documents/new_file.txt") as file:
+# with open("../../Documents/new_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
