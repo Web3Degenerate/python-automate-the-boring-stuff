c187af2fbf2fac13cb804e679be6478ada35e56e
web3degenerate <hostnomics@gmail.com>
2026-01-27 23:56:10 -1200

    Updated main.py

--- 24.Files-Directories-and-Paths/mail-merge/main.py
+++ 24.Files-Directories-and-Paths/mail-merge/main.py
@@ -6,3 +6,4 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
